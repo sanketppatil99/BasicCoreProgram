6cc51e76ebe995261fe18649485836df07b81486
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-15 19:50:16 +0530

    Print the year is a Leap Year or not

---
+++ leapYear.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class leapYear
+    {
+        public static void LeapYear()
+        {
+            int a = 400;
+            int b = 100;
+            int c = 4;
+            Console.WriteLine("Enter the year in Four digit =  ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            if ((userInput % a) == 0)
+            {
+                Console.WriteLine(" {0} is Leap Year ", userInput);
+            }
+            else if ((userInput % b) == 0)
+            {
+                Console.WriteLine(" {0} is not a leap year ", userInput);
+            }
+            else if ((userInput % c) == 0)
+            {
+                Console.WriteLine("{0} is leap Year ", userInput);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a leap Year", userInput);
+            }
+        }
+    }
+}
