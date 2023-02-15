94b1f3d5158238800b153dcfdb6d780505a73c90
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-15 20:22:19 +0530

    Print the Nth Harmonic Value

---
+++ HarmonicNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public static class HarmonicNumber
+    {
+        public static void harmonicNumber()
+        {
+            Console.WriteLine("Enter the harmonic Number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            double nth = 0.0;
+
+            if (number != 0)
+            {
+                for (double i = 1; i <= number; i++)
+                {
+                    nth = nth + 1.0 / i;
+
+                    Console.Write("1 / {0} + ",i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter the number greater than zero");
+            }
+        }
+    }
+
+}
