19a9f2d18c502b2cd54ffd11d43f808af8f3a717
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-15 20:22:45 +0530

    Calling method

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Programs");
-            PowerofTwo.powerofTwo();
+            HarmonicNumber.harmonicNumber();
         }
     }
 }
