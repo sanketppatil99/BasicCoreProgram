722bea726c32ed91a024007a176bdb5f5361a745
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-15 20:41:45 +0530

    Calling method

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Programs");
-            QuotientAndRemainder.FindTheQuotientAndRemainder();
+            Swap.SwapingTheNumber();
         }
     }
 }
