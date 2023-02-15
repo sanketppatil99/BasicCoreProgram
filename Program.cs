9cc76ace178e7d6c6351d06e734a9ba3ac5ff33f
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-15 19:42:18 +0530

    Calling method

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Programs");
+            flipCoin.CalculatePersentage();
         }
     }
 }
