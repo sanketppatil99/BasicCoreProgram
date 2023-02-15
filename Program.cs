cadcb52fe129ee6dbafaee4bc9d0a014d951edaf
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-15 20:30:50 +0530

    Calling method

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Programs");
-            HarmonicNumber.harmonicNumber();
+            Factor.primeFactor();
         }
     }
 }
